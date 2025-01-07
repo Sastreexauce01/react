bf22e0782bfc3dba8696485b91b9b166f10cc338
sastreexauce01 <sastreexauce01@gmail.com>
2025-01-07 15:08:32 +0100

    deuxieme commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import Banner from './Banner'
+import Carte from './Carte'
+
+
+
+function App(){
+   return(
+    <div>
+      <Banner />
+      <Carte />
+    </div>
+   )
+}
+
+export default App
