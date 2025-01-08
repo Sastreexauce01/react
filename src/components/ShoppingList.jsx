8e6e34bcd61908f1d6d722669c1e3bbd3c54af86
sastreexauce01 <sastreexauce01@gmail.com>
2025-01-08 14:08:25 +0100

    les props

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,5 +1,6 @@
 import { plantList } from "../datas/plantList";
 import "../styles/ShoppingList.css";
+import PlantItem from "./PlantItem";
 
 function ShoppingList() {
   const categories = plantList.reduce(
@@ -10,17 +11,21 @@ function ShoppingList() {
 
   return (
     <div>
-      <ul >
+      <ul>
         {categories.map((cat) => (
           <li key={cat}>{cat}</li>
         ))}
       </ul>
-      <ul className='lmj-plant-list'>
+
+      <ul className="lmj-plant-list">
         {plantList.map((plant) => (
-          <li key={plant.id} className="lmj-plant-item">
-            {plant.name}
-            {plant.isSpecialOffer && <div className="lmj-sales"> Soldes</div>}{" "}
-          </li>
+          <PlantItem
+            name={plant.name}
+            cover={plant.cover}
+            id={plant.id}
+            light={plant.light}
+            water={plant.water}
+          />
         ))}
       </ul>
     </div>
