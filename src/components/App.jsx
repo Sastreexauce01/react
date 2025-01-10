3d405c5d0600cc2412b449ea23c8011fb6bf168a
sastreexauce01 <sastreexauce01@gmail.com>
2025-01-10 12:28:41 +0100

    les state local

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
 import Banner from "./Banner";
 import Carte from "./Carte";
-import ShoppingList from "./ShoppingList.jsx";
-import QuestionForm from "./QuestionForm.jsx";
+import logo from "../assets/logo.png";
 import Footer from "./Footer.jsx";
+import ShoppingList from "./ShoppingList.jsx";
+import '../styles/Layout.css'
 
 function App() {
   return (
     <div>
-      <Banner />
-      <Carte />
-      <ShoppingList />
-      <QuestionForm />
+      <Banner>
+        <img src={logo} alt="La maison jungle" className="lmj-logo" />
+        <h1 className="lmj-title">La maison jungle</h1>
+      </Banner>
+      <div className="lmj-layout-inner">
+        <Carte />
+        <ShoppingList />
+      </div>
+
       <Footer />
     </div>
   );
--- src/components/Carte.jsx
+++ src/components/Carte.jsx
@@ -1,18 +1,37 @@
+
+import { useState } from 'react'
 import '../styles/Carte.css'
 
 function Carte() {
 	const monsteraPrice = 8
-	const ivyPrice = 10
-	const flowerPrice = 15
-	return (
-		<div className='imj-Carte'>
-			<h2 >Panier</h2>
-			<ul>
-				<li>Monstera : {monsteraPrice}€</li>
-				<li>Lierre : {ivyPrice}€</li>
-				<li>Fleurs : {flowerPrice}€</li>
-			</ul>
-			Total : {monsteraPrice + ivyPrice + flowerPrice}€
+	const [carte, updateCarte] = useState(0)
+	const [isOpen, setIsOpen] = useState(true)
+
+	return isOpen ? (
+		<div className='lmj-carte'>
+			<button
+				className='lmj-carte-toggle-button'
+				onClick={() => setIsOpen(false)}
+			>
+				Fermer
+			</button>
+			<h2>Panier</h2>
+			<div>Monstera : {monsteraPrice}€</div>
+			<button onClick={() => updateCarte(carte + 1)}>Ajouter</button>
+			<h3>Total : {monsteraPrice * carte}€</h3>
+
+			<button onClick={()=> updateCarte(0)}>
+				Vider le panier
+			</button>
+		</div>
+	) : (
+		<div className='lmj-carte-closed'>
+			<button
+				className='lmj-carte-toggle-button'
+				onClick={() => setIsOpen(true)}
+			>
+				Ouvrir le Panier
+			</button>
 		</div>
 	)
 }
